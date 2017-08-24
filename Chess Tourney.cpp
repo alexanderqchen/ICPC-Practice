ff3ae361d1bfd00e4bc7c0ad160a413386991797
alexanderqchen <room2210@yahoo.com>
2017-08-23 23:51:01 -0700

    Chess Tourney

---
+++ Chess Tourney.cpp
@@ -0,0 +1,21 @@
+//http://codeforces.com/problemset/problem/845/A
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> players(2 * n);
+	for (int i = 0; i < 2 * n; i++)
+	{
+		cin >> players[i];
+	}
+	sort(players.begin(), players.end());
+	if (players[players.size()/] > players[players.size() / 2-1])
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
